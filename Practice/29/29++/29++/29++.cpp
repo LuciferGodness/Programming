1f0297991d21d4ec3527b26bee0d803c48a167fb
LuciferGodness <btstheorys@gmail.com>
2020-12-24 18:13:25 +0300

    all

--- Practice/29/29++/29++/29++.cpp
+++ Practice/29/29++/29++/29++.cpp
@@ -4,6 +4,9 @@
 #include <iostream>
 #include<map>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
 using namespace std;
 
 struct students {
@@ -16,13 +19,94 @@ struct students {
                               {"prog", pr} };
 };
 
+template <class T>
+vector<T> BozoSort(vector<T> array, bool t = true)
+{
+    bool sorted = false;
+    int n = array.size();
+    while (!sorted)
+    {
+        if (t)
+        {
+            int index1 = rand() % n;
+            int index2 = rand() % n;
+            T temp = array[index1];
+            array[index1] = array[index2];
+            array[index2] = temp;
+            sorted = true;
+            for (int i = 1; i < n; i++)
+            {
+                if (array[i - 1] > array[i])
+                {
+                    sorted = false;
+                    break;
+                }
+            }
+        }
+        else
+        {
+            int index1 = rand() % n;
+            int index2 = rand() % n;
+            T temp = array[index1];
+            array[index1] = array[index2];
+            array[index2] = temp;
+            sorted = true;
+            for (int i = 1; i < n; i++)
+            {
+                if (array[i - 1] < array[i])
+                {
+                    sorted = false;
+                    break;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+ostream& operator << (ostream& out, students stud) {
+    out << "+------------+-------+------+------+------+------+\n"
+        << "| Name       | Group | Math | Phys | Hist | Prog |\n"
+        << "+------------+-------+------+------+------+------+\n"
+        << "|" << setw(11) << stud.name << " | " << stud.group << " "
+        << "    | " << stud.exams.at("math")
+        << "    | " << stud.exams.at("phys")
+        << "    | " << stud.exams.at("hist")
+        << "    | " << stud.exams.at("prog")
+        << "    |\n"
+        << "+------------+-------+------+------+------+------+\n";
+    return out;
+}
+
 ostream& operator << (ostream& out, vector<students> student)
 {
-    out 
+    out << "+------------+-------+------+------+------+------+\n"
+        << "| Name       | Group | Math | Phys | Hist | Prog |\n"
+        << "+------------+-------+------+------+------+------+\n";
+    for (const auto& stud : student) {
+        out << "|" << setw(11) << stud.name << " | " << stud.group << " "
+            << "    | " << stud.exams.at("math")
+            << "    | " << stud.exams.at("phys")
+            << "    | " << stud.exams.at("hist")
+            << "    | " << stud.exams.at("prog")
+            << "    |\n"
+            << "+------------+-------+------+------+------+------+\n";
+    }
+    return out;
 }
+
+bool operator > (students& a, students& b) {
+    return a.name > b.name;
+}
+
+bool operator < (students& a, students& b) {
+    return a.name < b.name;
+}
+
 int main()
 {
-    vector<students> Student
+    srand(time(NULL));
+    vector<students> student
     {
         students{"Malfoy D", 1, 5, 5, 5, 5},
         students{"Plov M", 1, 4, 3, 4, 4},
@@ -35,6 +119,25 @@ int main()
         students{"Mopedovna N", 4, 2, 3, 3, 2},
         students{"Dunya J", 2, 5, 5, 5, 4},
     };
+
+    vector<students> have_two;
+    for (int i = 0; i < 10; i++)
+    {
+        if (student[i].exams.at("math") == 2 or student[i].exams.at("phys") == 2 or student[i].exams.at("hist") == 2 or student[i].exams.at("prog") == 2)
+            have_two.push_back(student[i]);
+    }
+
+    if (have_two.empty())
+    {
+        cout << "Not found" << endl;
+        return 0;
+    }
+    else
+        cout << BozoSort(have_two);
+
+    cout << endl << "Expulsion" << endl;
+    int i = rand() % have_two.size();
+    cout << have_two[i];
 }
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
