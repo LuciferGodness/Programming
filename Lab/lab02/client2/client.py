c8316feeff565682c2cb72d4743c69fed0a80f33
LuciferGodness <btstheorys@gmail.com>
2020-12-27 19:06:44 +0300

    all and lab02

--- Lab/lab02/client2/client.py
+++ Lab/lab02/client2/client.py
@@ -1,57 +1,78 @@
-from flask import Flask, request
 import openpyxl
+from flask import Flask, request
 import datetime
 import json
 import os.path
 
-with open("data.txt", "r", encoding="utf-8") as f1, open("xl.txt", "w", encoding="utf-8") as f2:
-    print("".join(f1.readlines()), file=f2)
+def make_book(sheet):
+	sheet.cell(row = 1,column = 1).value = 'N'
+	sheet.cell(row = 1,column = 2).value = 'User ID'
+	sheet.cell(row = 1,column = 3).value = 'Datetime'
+	sheet.cell(row = 1,column = 4).value = 'Item'
+	sheet.cell(row = 1,column = 5).value = 'Price'
+	return sheet
 
-def excel():
-    global line, buffer
-    book = openpyxl.load_workbook('data.txt')
-    sheet= book.active
-    for items in buffer:
-        for i in range(5):
-            sheet.cell(line,i+1).value = items[i]
-        line+=1
-    sheet.cell(1,6).value = line-1
-    book.save('data.txt')
-    book.close
-    buffer.clear()
+
+
+def make_excel_file(): #тут записываем в наш эксель покупки и сохраняем в файл
+	global row_to_write_sells
+	global remember_our_sells
+	book = openpyxl.load_workbook('data.xlsx')
+	sheet = book.active
+	for sells in remember_our_sells:
+		for i in range(1,6):
+			sheet.cell(row_to_write_sells,i).value = sells[i]
+		row_to_write_sells+=1
+	for i in range (len(remember_our_sells)):
+			remember_our_sells.pop(i)
+	book.save('data.xlsx')
+	book.close
+
+
 app = Flask(__name__)  
-@app.route('/', methods = ['POST', 'GET'])
-def index():
-    if request.method == 'POST':
-        time = datetime.datetime.now().time()
-        json_from_post= request.get_json()
-        global number, buffer
-        for item in json_from_post['cart']:
-            buy = [number, json_from_post['user_id'], time, item['item'], item['price']]
-            number += 1  
-            buffer.append(buy)
-        if len(buffer)>1000:
-            excel()
-        return 'OK'
+
+
+@app.route('/', methods = ['POST', 'GET']) #декоратор
+def index():		#обработчик
+	our_json= request.get_json()
+	global N
+	global remember_our_sells
+	now_time = datetime.datetime.now().time()
+	for sells in our_json['check']:
+		temp.append(N)
+		temp.append(our_json['User_id'])
+		temp.append(now_time)
+		temp.append(sells['Item'])
+		temp.append(sells['Price'])
+		remember_our_sells.append(temp)
+		N += 1
+		N = str(N)
+	if len(remember_our_sells)>1000:
+		make_excel_file()
+	return 'OK'
+
+
 if __name__ == "__main__":
-    global number, buffer, line
-    number = 1
-    line = 2
-    buffer = []
-    if not(os.path.exists('data.txt')):    
-        book = openpyxl.Workbook()
-        sheet = book.active
-        sheet.cell(1,1).value = 'N'
-        sheet.cell(1,2).value = 'User ID'
-        sheet.cell(1,3).value = 'Datetime'
-        sheet.cell(1,4).value = 'Item'
-        sheet.cell(1,5).value = 'Price'
-        sheet.cell(1,6).value = 1
-        book.save('data.txt')
-        book.close
-else:
-    book = openpyxl.load_workbook('data.txt')
-    sheet = book.active
-    number = sheet.cell(1,6).value
-    book.close
-app.run()
+	global N
+	global remember_our_sells
+	global row_to_write_sells
+	row_to_write_sells = 2
+	remember_our_sells = []
+	N = str(1)
+	check_file = os.path.exists('data.xlsx')
+	if (check_file == False):  
+		book = openpyxl.Workbook()
+		sheet = book.active
+		sheet = make_book(sheet)
+		book.save('data.xlsx')
+		book.close
+
+	book = openpyxl.load_workbook('data.xlsx')
+	sheet = book.active
+	
+	now_time = datetime.datetime.now().time()
+
+
+	book.save('data.xlsx')
+	book.close
+	app.run()
---
+++ Practice/30/30++/30++/30++.cpp
@@ -0,0 +1,279 @@
+﻿// 30++.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+# include <iostream>
+# include <vector>
+# include <cstdlib>
+# include <ctime>
+# include <algorithm>
+# include <string>
+
+struct Coin {
+    int count;
+};
+
+struct Rune {
+    enum class Element { FIRE, WATER, EARTH, AIR } element;
+    int level;
+};
+
+std::string to_string(Rune::Element& element);
+
+struct Weapon {
+    int damage;
+    int critical;
+    int durability;
+};
+
+struct Armor {
+    int guard;
+    int durability;
+};
+
+struct Item;
+Item generateItem();
+
+struct Item
+{
+    enum class ItemType { COIN, RUNE, WEAPON, ARMOR } type;
+    union Value { Coin coin; Rune rune; Weapon weapon; Armor armor; } value;
+
+    Item& operator++()
+    {
+        *this = generateItem();
+        return *this;
+    }
+};
+
+Item GetCoin(int count);
+
+Item GetRune(Rune::Element element, int level);
+Item GetFireRune(int level);
+Item GetWaterRune(int level);
+Item GetEarthRune(int level);
+Item GetAirRune(int level);
+
+Item GetWeapon(int damage, int critical, int durability);
+Item GetArmor(int guard, int durability);
+
+template<class T, class S>
+struct Pair
+{
+    T first;
+    S second;
+};
+
+std::vector<Pair<Item, double>> chances{
+    {GetCoin(1000), 50.0},
+    {GetFireRune(1), 6.0},
+    {GetWaterRune(1), 13.0},
+    {GetEarthRune(1), 7.0},
+    {GetAirRune(1), 14.0},
+    {GetFireRune(5), 0.6},
+    {GetWaterRune(5), 1.3},
+    {GetEarthRune(5), 0.7},
+    {GetAirRune(5), 1.4},
+    {GetFireRune(5), 0.06},
+    {GetWaterRune(5), 0.13},
+    {GetEarthRune(5), 0.07},
+    {GetAirRune(5), 0.14},
+    {GetWeapon(100, 0, 100), 1.4},
+    {GetWeapon(75, 50, 100), 1.4},
+    {GetArmor(50, 100), 2.8},
+};
+
+const int precision = 100;  // Точность до сотых в шансах
+double count_total_chance() {
+    double sum = 0;
+    for (auto& pair : chances) {
+        sum += pair.second;
+    }
+    return sum * precision;
+}
+
+const double total_chance = count_total_chance();  // сумма всех шансов
+
+using LootBox = std::vector<Item>;
+
+LootBox generateLootBox();
+
+LootBox& operator<<(LootBox& lootbox, Item& item);
+
+std::ostream& operator<<(std::ostream& out, Item& item);
+
+std::ostream& operator<<(std::ostream& out, LootBox& lootbox);
+
+// std::string toLowerCase(std::string str);
+
+// std::string toLowerCase(std::string data) {
+//     return std::transform(data.begin(), data.end(), data.begin(),
+//     [](unsigned char c){ return std::tolower(c); });
+// }
+
+LootBox& operator<<(LootBox& lootbox, Item item) {
+    lootbox.push_back(item);
+    return lootbox;
+}
+
+Item generateItem() {
+    double r = double(rand() % int(total_chance)) / 100; // [0.00; 100.00)
+    double current_sum = 0;
+
+    for (auto& elem : chances) {
+        if (current_sum <= r and r < current_sum + elem.second) {
+            return elem.first;
+        }
+        current_sum += elem.second;
+    }
+    return chances[0].first;
+}
+
+LootBox generateLootBox() {
+    LootBox box;
+    std::srand(std::time(0));
+
+    for (int i = 0; i < 3; ++i) {
+        box << generateItem();
+    }
+    return box;
+}
+
+std::ostream& operator<<(std::ostream& out, Item& item) {
+    Item::ItemType& type = item.type;
+
+    if (type == Item::ItemType::COIN) {
+        out << item.value.coin.count << " gold";
+    }
+    else if (type == Item::ItemType::RUNE) {
+        Rune& rune = item.value.rune;
+        out << "Rune of " << to_string(rune.element)
+            << " level " << rune.level;
+    }
+    else if (type == Item::ItemType::WEAPON) {
+        Weapon& weapon = item.value.weapon;
+        out << "Weapon {damage: " << weapon.damage
+            << ", critical: " << weapon.critical
+            << ", durability: " << weapon.durability
+            << "}";
+    }
+    else if (type == Item::ItemType::ARMOR) {
+        Armor& armor = item.value.armor;
+        out << "Armor {guard: " << armor.guard
+            << ", durability: " << armor.durability
+            << "}";
+    }
+    out << std::flush;
+    return out;
+}
+
+std::ostream& operator<<(std::ostream& out, LootBox& lootbox) {
+    for (auto& elem : lootbox) {
+        out << elem << "\n" << std::flush;
+    }
+    return out;
+}
+
+std::string to_string(Rune::Element& element) {
+    std::string result;
+
+    switch (element) {
+    case Rune::Element::FIRE:
+        result = "fire";
+        break;
+    case Rune::Element::WATER:
+        result = "water";
+        break;
+    case Rune::Element::EARTH:
+        result = "earth";
+        break;
+    case Rune::Element::AIR:
+        result = "air";
+        break;
+    }
+
+    return result;
+}
+
+Item GetCoin(int count) {
+    return {
+        Item {
+            Item::ItemType::COIN,
+            Item::Value {.coin = Coin{count}}
+        }
+    };
+}
+Item GetRune(Rune::Element element, int level) {
+    return {
+        Item {
+            Item::ItemType::RUNE,
+            Item::Value {
+                .rune = Rune {
+                    element,
+                    level
+                }
+            }
+        }
+    };
+}
+Item GetFireRune(int level) {
+    return GetRune(Rune::Element::FIRE, level);
+}
+Item GetWaterRune(int level) {
+    return GetRune(Rune::Element::WATER, level);
+}
+Item GetEarthRune(int level) {
+    return GetRune(Rune::Element::EARTH, level);
+}
+Item GetAirRune(int level) {
+    return GetRune(Rune::Element::AIR, level);
+}
+Item GetWeapon(int damage, int critical, int durability) {
+    return {
+        Item {
+            Item::ItemType::WEAPON,
+            Item::Value{.weapon = Weapon{damage, critical, durability}}
+        }
+    };
+}
+Item GetArmor(int guard, int durability) {
+    return {
+        Item {
+            Item::ItemType::ARMOR,
+            Item::Value{.armor = Armor{guard, durability}}
+        }
+    };
+}
+
+using namespace std;
+int main()
+{
+    setlocale(0, "");
+    string line;
+    cout << "Открыть лутбокс? Yes/No" << endl;
+    cin >> line;
+    if (line == "Y" or line == "yes" or line == "y" or line == "YES" or line == "Yes")
+    {
+        cout << "Молодес" << endl;
+        return main();
+    }
+    else if (line == "NO" or line == "N" or line == "n" or line == "no" or line == "No")
+    {
+        return 0;
+    }
+    else
+    {
+        return main();
+    }
+}
+
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл
